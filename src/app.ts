cec81e8dbf2ab95e7311dd3b99c58b607d8c765c
Vincent Bamps <vincent.bamps@fednot.be>
2020-02-25 16:36:43 +0100

    vba-redux: setting reducers

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,36 @@
-import { renderTodos } from './utils';
+import {renderTodos} from './utils';
+import * as fromStore from './store'
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+const reducers = {
+  todos: fromStore.reducer
+};
+const store = new fromStore.Store(reducers)
+
+console.log(store.value)
 
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const payload = {label: input.value, complete: false};
+    store.dispatch({
+      type:'ADD_TODO',
+      payload: payload
+    })
 
-    console.log(payload);
+    console.log(store.value);
 
     input.value = '';
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+todoList.addEventListener('click', function (event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
     console.log(target);
---
+++ src/store/store.ts
@@ -0,0 +1,29 @@
+export class Store{
+  private subscribers: Function[];
+  private reducers: {[key: string]: Function}
+  private state: {[key:string]: any};
+
+  constructor(reducers = {}, initialState = {}) {
+    this.reducers = reducers;
+    this.state = this.reduce(initialState,{});
+  }
+
+  get value(){
+    return this.state;
+  }
+
+  dispatch(action){
+    this.state = this.reduce(this.state, action);
+  }
+
+  private reduce(state, action){
+    const newState = {}
+    for(const prop in this.reducers) {
+      //newState.todos = this.reducers.todos()
+      // we don't want to pass the whole state, don't let access to it.
+      newState[prop] = this.reducers[prop](state[prop], action);
+    }
+    return newState;
+
+  }
+}
